80431d0bb5cd2f9a107411e5d2bd2ed4dff92a39
Sidney Roberto <sidneyrpsilva@gmail.com>
2023-11-05 16:12:36 -0300

    📦 refactor: Add Header link nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
 import { GithubButton } from '@/components/GithubButton';
 import { ThemeButton } from '@/components/ThemeButton';
+import Link from 'next/link';
 
 export function Header() {
   return (
     <header className="py-4 flex items-center justify-between">
-      <div className="font-bold text-2xl">
-        <h1>WINDRED</h1>
-      </div>
+      <Link
+        href={'/'}
+        prefetch={false}
+        className="bg-stone-900 dark:bg-stone-100 text-stone-100 dark:text-stone-900 rounded-2xl">
+        <h1 className="p-2 px-3 font-bold text-xl select-none">W</h1>
+      </Link>
 
       <div className="flex items-center justify-center gap-4">
         <ThemeButton />
